5e6280fb4f902b8a1555bdb646cc29bb4c8df1a0
Tejeshwini-M <tejeshwini73@gmail.com>
2026-04-07 08:39:24 +0530

    save

---
+++ auth-token-lab/src/components/Login.jsx
@@ -0,0 +1,15 @@
+const Login = ({ onLogin }) => {
+ const handleLogin = () => {
+ const fakeToken = "fake-jwt-token-123"; // simulated token
+ localStorage.setItem("authToken", fakeToken);
+// sessionStorage.setItem("authToken", fakeToken);
+ onLogin();
+ };
+ return (
+ <div>
+ <h2>Login Page</h2>
+ <button onClick={handleLogin}>Login</button>
+ </div>
+ );
+};
+export default Login;
